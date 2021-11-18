40f7834d62d58d0e74d9b5d4e3494c0e4fb16651
Milan <milan@MDMBA.local>
2021-11-18 22:25:00 +0100

    added Enter in app.js

--- app1/app.js
+++ app1/app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
 if (req.url === '/') {
-res.write('Hello from Node.js app 1');
+res.write('Hello from Node.js app 1 \n');
 res.write('This service runs on port 5000 on the server');
 res.end();
 }
