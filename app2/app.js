5e5a1fb4d20db2ac64e5c6002b645676adfc83f1
Milan <milan@MDMBA.local>
2021-11-18 22:19:21 +0100

    changed app2

--- app2/app.js
+++ app2/app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
 if (req.url === '/') {
-res.write('Hello from Node.js app 1');
+res.write('Hello from Node.js app 2');
 res.end();
 }
 });
