38cbbadcf385256743d0854c501bfba123b036fc
milan <milan.dima@vives.be>
2024-11-17 16:23:28 +0100

    removed version from docker-compose.yaml

--- app1/app.js
+++ app1/app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
 if (req.url === '/') {
 res.write('Hello from Node.js app 1 \n');
-res.write('This service runs on port 5000 on the server');
+res.write('This service runs on port 5001 on the server');
 res.end();
 }
 });
